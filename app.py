d748bafe16d5e79eaa55f2813d88df98a718b756
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-09-17 19:42:13 +0000

    commit final

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask, render_template, request   # import flask framework
-app = Flask(__name__)             # create an app instance
+from flask import Flask, render_template, request   
+app = Flask(__name__)             
 
-@app.route("/")                   # use the home url
-def hello():                      # method called hello
-    return render_template("index.html")         # returns "hello world"
-
-@app.route("/<name>")              # route with URL variable /<name>
-def hello_name(name):              # call method hello_name
-    return "Hello "+ name          # which returns "hello + name  
+@app.route("/")                   
+def hello():                      
+    return render_template("index.html")         
 
 @app.route("/about")
 def about():
-    name = request.args.get('name') if request.args.get('name') else "Hello World!" 
-    return render_template("about.html", aboutName=name) 
- 
-if __name__ == "__main__":        # when running python app.py
-    app.run(debug=True)                     # run the flask app
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":        
+    app.run(debug=True)                     
 
 
